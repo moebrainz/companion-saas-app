8dc9fd3b78d93e1d4451e4674e4b6c7fe197248e
moebrainz <moebrainz@gmail.com>
2025-10-21 00:56:18 +0100

    implement companion dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h2 className="text-3xl">Hello there</h2>
-      <Button>Whats up</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Something Light"
+          topic="Brain Functionality"
+          subject="science"
+          duration={23}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="355"
+          name="Woods and Uses"
+          topic="Wook Work Technology"
+          subject="basic science"
+          duration={43}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="533"
+          name="Something Heavy"
+          topic="Brain Malfunctionality"
+          subject="science"
+          duration={33}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   );
 };
 
